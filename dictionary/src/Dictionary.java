c733832775667c4deb9c1d7c3adbb3918463c590
vdominika <vdominika@yahoo.com>
2017-09-18 17:23:47 +0200

    Dictionary

---
+++ dictionary/src/Dictionary.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class Dictionary{
+    static Map<String,String> map;
+
+    Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        map = new HashMap<String,String>();
+        map.put("alma", "apple");
+        map.put("fa", "tree");
+    }
+
+    // Implement this method. It should add the given key-value pair to the the map
+    public static void addWord(String hunWord, String engWord) {
+
+    }
+
+    // Implement this method. It should remove the key-value pair by the given key from the map
+    public static void removeWord(String hunWord) {
+
+    }
+
+    // Implement these methods. They should return the translation of the given word from the map
+    public static String translateToHun(String engWord) {
+        return null;
+    }
+
+    public static String translateToEng(String hunWord) {
+        return null;
+    }
+}
+
